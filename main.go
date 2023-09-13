82f56658dbeede85f3974813e8fa6c4274ee350a
jdcukier <jcukier@live.com>
2023-09-12 21:52:31 -0700

    Cleaned up code a bit to make it easier to expand

--- main.go
+++ main.go
@@ -13,19 +13,30 @@ import (
 // If the web application needs to be accessible to the internet, you will need to open port 3000.
 const port = 3000
 
-// Runs the server until you kill the process.
+// Runs the server, listening for requests until you kill the process.
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", index)
-	r.Get("/reh", basicRehsponse)
+	rehgister(r)
 	http.ListenAndServe(fmt.Sprintf(":%v", port), r)
 }
 
+// Mux Registration
+
+// rehgister the middleware and endpoints to the mux router.
+func rehgister(mux *chi.Mux) {
+	mux.Use(middleware.Logger)
+	mux.Get("/", index)
+	mux.Get("/reh", basicRehsponse)
+}
+
+// Endpoint Handlers
+
+// basicRehsponse is a sample endpoint, just writes back a string to the requestor.
 func basicRehsponse(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("reh"))
 }
 
+// index serves the static index.html file
 func index(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("TBD - figuring out how to serve a file"))
 }
